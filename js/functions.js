eb737c48e42402953e2519922fd1febf50d27790
KinshinyerR <kinshinyer02@gmail.com>
2021-03-19 02:18:19 -0500

    Functions

--- js/functions.js
+++ js/functions.js
@@ -5,6 +5,14 @@ let modoStorage = localStorage.getItem("modo");
 let arraySearch = [];
 let searchGifs;
 
+const modal = document.querySelector(".modal-section");
+let left = document.querySelector(".left");
+let right = document.querySelector(".right");
+let imagen = document.querySelector("#imagen");
+let close = document.querySelector(".close");
+let info = document.querySelector(".info");
+let cont = 0;
+
 if (localStorage.getItem("favList")) {
   favList = JSON.parse(localStorage.getItem("favList"));
 }
@@ -294,4 +302,72 @@ function suggested(palabra) {
       });
     })
     .catch((error) => console.log(error));
+}
+
+function slider(contenedor, img, indice, gifos) {
+  cont = indice;
+  imagen.src = img[cont];
+  updateInfo(gifos);
+  contenedor.addEventListener("click", (e) => {
+    let tgt = e.target;
+
+    if (tgt == left) {
+      if (cont > 0) {
+        imagen.src = img[cont - 1];
+        cont--;
+      } else {
+        imagen.src = img[img.length - 1];
+        cont = img.length - 1;
+      }
+    } else if (e.target == right) {
+      if (cont < img.length - 1) {
+        imagen.src = img[cont + 1];
+        cont++;
+      } else {
+        imagen.src = img[0];
+        cont = 0;
+      }
+    }
+    updateInfo(gifos);
+  });
+}
+
+function updateInfo(gifos) {
+  info.innerHTML = `
+      <div class="infoGif">
+        <p class="title">${gifos[cont].username}</p>
+        <p class="paragraph">${gifos[cont].title}</p>
+      </div>
+      <div class="icons">
+          <img id="iconFav" src="img/icon-fav.svg" alt="" srcset="">
+          <img id="iconDown" src="img/icon-download.svg" alt="" srcset="">
+      </div>
+    `;
+
+  const imgFav = info.children[1].children[0];
+  const imgDow = info.children[1].children[1];
+
+  const found = favList.findIndex((elemento) => elemento.id === gifos[cont].id);
+
+  if (found !== -1) {
+    imgFav.src = "img/icon-fav-active.svg";
+  }
+
+  imgFav.addEventListener("click", () => {
+    const found = favList.findIndex(
+      (elemento) => elemento.id === gifos[cont].id
+    );
+    saveFav(gifos[cont]);
+    if (found !== -1) {
+      imgFav.src = "img/icon-fav.svg";
+    } else {
+      imgFav.src = "img/icon-fav-active.svg";
+    }
+  });
+  imgDow.addEventListener("click", () => {
+    downloadGif(gifos[cont].images.original.url, gifos[cont].title);
+  });
+  close.addEventListener("click", () => {
+    modal.classList.toggle("active");
+  });
 }
