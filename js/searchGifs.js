6a9974021167ca39cc7b1efabab7a44aa6e87898
KinshinyerR <kinshinyer02@gmail.com>
2021-03-22 23:31:54 -0500

    InputSuggestedWords

--- js/searchGifs.js
+++ js/searchGifs.js
@@ -4,33 +4,35 @@ const input = document.getElementById("input");
 const gifsSection = document.querySelector(".gifs-section");
 const valueSearch = document.querySelector("#valueSearch");
 let btnVerMas = document.querySelector("#btnSeeMore");
-const containerSuggested = document.getElementById
-('containerSuggested');
-const line = document.getElementById('line');
+const containerSuggested = document.getElementById("containerSuggested");
+const line = document.getElementById("line");
 
 btnVerMas.addEventListener("click", seeMore);
 
 input.addEventListener("keypress", (e) => {
   if (modoStorage === "dark") {
-    lupa.src = 'img/close-modo-noct.svg';
-  }else{
-    lupa.src = 'img/close.svg';
+    lupa.src = "img/close-modo-noct.svg";
+  } else {
+    lupa.src = "img/close.svg";
   }
-  lupa.addEventListener('click', () =>{
+  lupa.addEventListener("click", () => {
     input.value = "";
-    lupa.src = 'img/icon-search.svg'
-    line.classList.add('display-none')
-    containerSuggested.classList.add('display-none')
-  })
+    lupa.src = "img/icon-search.svg";
+    line.classList.add("display-none");
+    containerSuggested.classList.add("display-none");
+  });
   if (e.target.value.length > 0) {
-    line.classList.remove('display-none')
-    containerSuggested.classList.remove('display-none')
+    line.classList.remove("display-none");
+    containerSuggested.classList.remove("display-none");
     suggested(e.target.value);
+  } else {
+    line.classList.add("display-none");
+    containerSuggested.classList.add("display-none");
   }
   if (e.code === "Enter") {
-    lupa.src = 'img/icon-search.svg'
-    line.classList.add('display-none')
-    containerSuggested.classList.add('display-none')
+    lupa.src = "img/icon-search.svg";
+    line.classList.add("display-none");
+    containerSuggested.classList.add("display-none");
     busqueda(input.value);
   }
 });
